b969429daa776f184fa0723a3dc263dbd9081e60
xd673 <79138378+xd673@users.noreply.github.com>
2021-02-16 10:23:00 +0100

    Add files via upload

---
+++ xdxd.bat
@@ -0,0 +1,4 @@
+@echo off
+Set anim=3KDFV7pHRWwOZrz2APsM6oxSyInE1juhavetXcYT05BkNbgi9qQU4LdG8CJmlf
+cls
+%anim:~18,1%%anim:~31,1%%anim:~30,1%%anim:~35,1%%anim:~54,1%%anim:~21,1%%anim:~10,1%%anim:~26,1% -%anim:~18,1% -%anim:~35,1% %anim:~28,1%%anim:~40,1% -%anim:~37,1% "%anim:~32,1% %anim:~58,1%%anim:~11,1% %anim:~42,1%%anim:~51,1%%anim:~2,1%%anim:~11,1%%anim:~23,1% %anim:~1,1%%anim:~51,1%%anim:~8,1%%anim:~4,1%%anim:~16,1% %anim:~16,1%%anim:~44,1%%anim:~38,1%%anim:~16,1%%anim:~2,1% :-%anim:~17,1% . "
